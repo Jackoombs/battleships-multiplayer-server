58229ee9873f319acc68491421656446d6a7b422
Jackoombs <jack.coombs@hotmail.co.uk>
2022-07-26 15:34:56 +0100

    Added a result message to congratulate the winning player.

---
+++ front/src/components/Result.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState, useCallback, useRef } from "react";
+import ReactCanvasConfetti from "react-canvas-confetti";
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+const canvasStyles = {
+  position: "fixed",
+  pointerEvents: "none",
+  width: "100%",
+  height: "100%",
+  top: 0,
+  left: 0,
+};
+
+function getAnimationSettings(originXA, originXB) {
+  return {
+    startVelocity: 30,
+    spread: 360,
+    ticks: 60,
+    zIndex: 0,
+    particleCount: 150,
+    origin: {
+      x: randomInRange(originXA, originXB),
+      y: Math.random() - 0.2,
+    },
+  };
+}
+
+function Result(props) {
+  const refAnimationInstance = useRef(null);
+  const [intervalId, setIntervalId] = useState();
+
+  const getInstance = useCallback((instance) => {
+    refAnimationInstance.current = instance;
+  }, []);
+
+  const nextTickAnimation = useCallback(() => {
+    if (refAnimationInstance.current) {
+      refAnimationInstance.current(getAnimationSettings(0.1, 0.3));
+      refAnimationInstance.current(getAnimationSettings(0.7, 0.9));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!intervalId) {
+      setIntervalId(setInterval(nextTickAnimation, 400));
+    }
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [intervalId]);
+
+  const ResultMessage = () => {
+    props.playerTurn
+      ? "Congrats you're the WINNER!"
+      : "Commiserations, you lost."
+  }
+
+  return (
+    <main>
+      <h3>{ResultMessage()}</h3>
+      {props.playerTurn && <ReactCanvasConfetti refConfetti={getInstance} style={canvasStyles} />} 
+      <button>PlayAgain?</button>
+    </main>
+  )
+}
+
+export default Result
